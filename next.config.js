949cbcc9e64e910d4db2fd7eae0207c9e3929bf6
techguywebdev-cmyk <techguywebdev@gmail.com>
2026-06-03 18:58:17 +0000

    Configure image domains and ESLint settings

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ['image.tmdb.org'],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
 
 module.exports = nextConfig;
